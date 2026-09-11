c14601e463d99d8d90eaa43a97724c6a84a586aa
mhvnsnt <fmmg.mars@gmail.com>
2026-09-10 22:09:02 -0500

    OSS umbrella: vendor complete upstream Git histories

--- scripts/production/install-oss-umbrella.sh
+++ scripts/production/install-oss-umbrella.sh
@@ -5,9 +5,9 @@ VENDOR="$ROOT/.trippedd/oss"
 mkdir -p "$VENDOR"
 clone_or_update() {
   local id="$1" url="$2" ref="$3" dir="$VENDOR/$1"
-  if [ ! -d "$dir/.git" ]; then git clone --filter=blob:none --no-tags "$url" "$dir"; fi
-  git -C "$dir" fetch --depth=1 origin "$ref" 2>/dev/null || true
-  git -C "$dir" fetch --depth=1 --tags origin 2>/dev/null || true
+  if [ ! -d "$dir/.git" ]; then git clone --no-tags "$url" "$dir"; fi
+  git -C "$dir" fetch origin "$ref"
+  git -C "$dir" fetch --tags origin
   git -C "$dir" checkout --detach "$ref"
 }
 clone_or_update comfyui https://github.com/Comfy-Org/ComfyUI.git master
